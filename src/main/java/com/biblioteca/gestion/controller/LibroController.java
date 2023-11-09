cf9153ee1735ca68d728d3b105ec25b644517d9c
Caro Bima <carobima@gmail.com>
2023-11-09 04:49:08 -0300

    Cargando documentacion en javadoc

--- src/main/java/com/biblioteca/gestion/controller/LibroController.java
+++ src/main/java/com/biblioteca/gestion/controller/LibroController.java
@@ -8,12 +8,19 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controller para gestionar las altas, bajas, modificaciones y borrado de libros
+ */
 @RestController
 @RequestMapping("/libro")
 public class LibroController {
     @Autowired
     ILibroService libroService;
 
+    /**
+     * Devuelve el listado de el catálogo de libros completo
+     * @return Lista de libros
+     */
     @GetMapping("/listarlibros")
     public List<Libro> listarLibros(){
         return libroService.findAll();
@@ -30,29 +37,54 @@ public class LibroController {
         return libroService.guardarLibro(nuevoLibro);
     }
 
+    /**
+     * Realiza la edición de un libro
+     * @param libroEditado
+     * @return libro ya editado que fue guardado en la base de datos
+     */
     @PutMapping("/editar")
     public Libro editarLibro(@RequestBody Libro libroEditado){
         return libroService.editarLibro(libroEditado);
     }
 
+    /**
+     * Permite realizar el borrado de un libro cuyo id se pasa por parametro
+     * @param id
+     * @return String indicando que el libro ha sido borrado
+     */
     @DeleteMapping("/borrar/{id}")
     public String borrarLibro(@PathVariable Long id){
         libroService.borrarLibro(id);
         return "Libro borrado";
     }
 
+    /**
+     * Permite realizar la búsqueda de un libro por el título del mismo
+     * @param titulo
+     * @return listado de libros cuyo titulo coinciden con el buscado
+     */
     @GetMapping("/busqueda/titulo/{titulo}")
     public List<Libro> buscarPorTitulo(@PathVariable String titulo) {
 
         return libroService.findLibroTitulo(titulo);
     }
 
+    /**
+     * Permite realizar la búsqueda de un libro por el autor del mismo
+     * @param autor
+     * @return listado de libros cuyo autor coinciden con el buscado
+     */
     @GetMapping("/busqueda/autor/{autor}")
     public List<Libro> buscarPorAutor(@PathVariable String autor) {
 
         return libroService.findLibroAutor(autor);
     }
 
+    /**
+     * Permite realizar la búsqueda de un libro por el género al que pertenece
+     * @param genero
+     * @return listado de libros pertenecientes al género buscado
+     */
     @GetMapping("/busqueda/genero/{genero}")
     public List<Libro> buscarPorGenero(@PathVariable String genero) {
 
--- src/main/java/com/biblioteca/gestion/controller/PrestamoController.java
+++ src/main/java/com/biblioteca/gestion/controller/PrestamoController.java
@@ -10,27 +10,54 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * Controlador para gestionar los prestamos de libros.
+ * Permite listar los prestamos registrados en la base de datos, registrar un nuevo prestamo y buscar préstamos
+ * por fecha de vencimiento o por usuario
+ */
+
 @RestController
 @RequestMapping("/prestamo")
 public class PrestamoController {
 
     @Autowired
     IPrestamoService prestamoService;
+
+    /**
+     * Devuelve el listado de prestamos registrados en la base de datos
+     * @return Listado de prestamos
+     */
     @GetMapping("/listarPrestamos")
     public List<Prestamo> listarLibros(){
         return prestamoService.findAll();
     }
 
+    /**
+     * Permite registrar un nuevo prestamo
+     * @param nuevoPrestamo
+     * @return prestamo que fue registrado en el sistema
+     */
     @PostMapping("/nuevo")
     public Prestamo crearPrestamo(@RequestBody Prestamo nuevoPrestamo){
         return prestamoService.registrarPrestamo(nuevoPrestamo);
     }
+
+    /**
+     * Permite realizar la busqueda de prestamos cuya fecha de vencimiento coincide con la pasada por parámetro
+     * @param fechaVencimiento
+     * @return listado de prestamos que vencen en la fecha consultada
+     */
 
     @GetMapping("/busqueda/fvencimiento/{fechaVencimiento}")
     public List<Prestamo> buscarPorFechaVenc(@PathVariable LocalDate fechaVencimiento){
         return prestamoService.consultarPrestamosPorFechaVenc(fechaVencimiento);
     }
 
+    /**
+     * Busqueda de prestamos realizados por un determinado usuario
+     * @param usuario
+     * @return listado de prestamos solicitados por un usuario puntual
+     */
     @PostMapping("/busqueda/usuario")
     public List<Prestamo> buscarPorUsuario(@RequestBody Usuario usuario){
         return prestamoService.consultrarPrestamosPorUsuario(usuario);
